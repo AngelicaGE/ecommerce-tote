85ffaa4648b6adde7bbff0d0cadc9cb8c542ebf8
unknown <angelicaguemes@hotmail.com>
2022-02-13 21:34:17 -0600

    [ADD] side navbar completed with functionality, pending create components for few links

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import '../styles/SideNavbar.scss'
 import SandwichMenu from '../containers/SandwichMenu';
+import { NavLink } from 'react-router-dom'
+
 
 
 const SideNavbar = ({clickOnMenu}) => {
   return (
   <div className='SideNavbar'>
-        <div className='sandwich-menu hide-in-laptop' onClick={clickOnMenu}>
-            <SandwichMenu isSelected={true}/>
+        <div className='sandwich-menu-open hide-in-laptop' onClick={clickOnMenu}>
+            <SandwichMenu className="SandwichMenu" isSelected={true}/>
+            <ul className='tote-pages-side' >
+                    <li className='tote-page'>
+                        <NavLink to="fghj" className={({isActive}) => (isActive? 'activeClass': '')}>New arrivals</NavLink>
+                    </li>
+                    <li className='tote-page'>
+                        <NavLink to='fgj' className={({isActive}) => (isActive? 'activeClass': '')}>Sale</NavLink>
+                    </li>
+                    <li className='tote-page favs'>
+                        <NavLink to="/favs" className={({isActive}) => isActive? 'activeClass': ''}>
+                            My Favorities
+                        </NavLink>                    
+                    </li>
+                    <li className='tote-page'>
+                        <NavLink to="/about" className={({isActive}) => isActive? 'activeClass': ''}>
+                            About
+                        </NavLink>                    
+                    </li>
+                </ul>
+        </div>
+        <div className='image-container'>
+          <div className='image'></div>
         </div>
   </div>
   );
