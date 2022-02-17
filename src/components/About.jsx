519ff2d6851134cb9b9514ef6f98dc04fe980273
unknown <angelicaguemes@hotmail.com>
2022-02-17 17:45:26 -0600

    [FIREBASE] grab all collections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InfoBlock from '../containers/InfoBlock';
 import selfie from '../assets/images/angie.jpeg'
+import {collection, doc, getDoc, getDocs, getFirestore} from "firebase/firestore"
 
 const About = () => {
+  const [generalInfo, setGeneralInfo] = useState([]);
+  
+  // ACCESS A SINGLE COLECTION 
+  /*
+  useEffect(() => {
+    const db = getFirestore();
+    const bloqueRef = doc(db, "bloque", "TONPLjjTbmedT8s3EqxE");
+    getDoc(bloqueRef).then( (snapshot) => {
+      console.log(snapshot.exists());
+      console.log(snapshot.id);
+      console.log(snapshot.data());
+      setGeneralInfo({
+        id: snapshot.id, 
+        ...snapshot.data()}
+      )
+    })
+  }, [])*/
+
+  useEffect(async () =>  {
+    console.log("hola")
+    const db = getFirestore();
+    const querySnapshot = await getDocs(collection(db, "bloque"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      let id = doc.id; 
+      let item = {id, ...doc.data()};
+      setGeneralInfo([
+        ...generalInfo,
+        item
+      ]);
+    });
+    console.log(generalInfo)
+    /*infoBlock.get().then((querySnapshot) => {
+      if(querySnapshot.size == 0){
+        console.log("no results");
+      }
+      setGeneralInfo(querySnapshot.docs.map(doc => doc.data()));
+    }).catch((error) => {
+      console.log("Error searching blocks", error)
+    }).finally(() => {
+      console.log("Finished grabing blokcs")
+      console.log(infoBlock)
+    });*/
+  }, [])
+  
+
+
   return (
     <div>   
         <h1> Hi, hope you like this book e-commerce.</h1>
-        <InfoBlock
-          title="About me, the developer"
-          subtitle="algun subtitulo"
-          image={selfie}
-          content="un poquito sobre mi mis gustos areas y demas"
-        ></InfoBlock>
-        <hr />
-        <InfoBlock
-          title="Mi deseo por incursionar mi carrera a web"
-          subtitle="Hola Coderhouse"
-          image={selfie}
-          content="Porque como cuando coderhouse? "
-        ></InfoBlock>
-        <hr />
-        <InfoBlock
-          title="Esta vivo!"
-          subtitle="Mi primer proyecto de React"
-          image={selfie}
-          content="Como nacio el e-commerce explicar que es el proyetco desarrollado en Coderhouse..."
-        ></InfoBlock>
-
+        {
+          generalInfo.map(block =>(
+            <InfoBlock
+              title={block.titulo}
+              subtitle={block.subtitulo}
+              image={selfie}
+              content={block.contenido}
+            ></InfoBlock>
+          ))
+        }
     </div>
   )
 }
