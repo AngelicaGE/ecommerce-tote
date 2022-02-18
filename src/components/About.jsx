73dac942ebd2e471177f6e120281103bab812859
unknown <angelicaguemes@hotmail.com>
2022-02-17 19:50:05 -0600

    [ADD] minor css changes to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,62 +5,39 @@ import {collection, doc, getDoc, getDocs, getFirestore} from "firebase/firestore
 
 const About = () => {
   const [generalInfo, setGeneralInfo] = useState([]);
-  
-  // ACCESS A SINGLE COLECTION 
-  /*
-  useEffect(() => {
-    const db = getFirestore();
-    const bloqueRef = doc(db, "bloque", "TONPLjjTbmedT8s3EqxE");
-    getDoc(bloqueRef).then( (snapshot) => {
-      console.log(snapshot.exists());
-      console.log(snapshot.id);
-      console.log(snapshot.data());
-      setGeneralInfo({
-        id: snapshot.id, 
-        ...snapshot.data()}
-      )
-    })
-  }, [])*/
 
-  useEffect(async () =>  {
+  useEffect( () =>  {
     console.log("hola")
     const db = getFirestore();
-    const querySnapshot = await getDocs(collection(db, "bloque"));
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      let id = doc.id; 
-      let item = {id, ...doc.data()};
-      setGeneralInfo([
-        ...generalInfo,
-        item
-      ]);
-    });
-    console.log(generalInfo)
-    /*infoBlock.get().then((querySnapshot) => {
-      if(querySnapshot.size == 0){
-        console.log("no results");
+    let tmpArray = [];
+    getDocs(collection(db, "bloque")).then((querySnapshot)=>{
+      querySnapshot.forEach((doc) => {
+        let isInArray = tmpArray.some(item => item.id === doc.id);
+        if(!isInArray){
+          tmpArray = [...tmpArray, {"id": doc.id, ...doc.data()}];
+          console.log("Aded to state: " + doc.id)
+        }
+      });
+    }).catch((error) =>{
+      console.log(error)
+    }).finally( () =>{
+        setGeneralInfo(tmpArray)
+        console.log(generalInfo)
       }
-      setGeneralInfo(querySnapshot.docs.map(doc => doc.data()));
-    }).catch((error) => {
-      console.log("Error searching blocks", error)
-    }).finally(() => {
-      console.log("Finished grabing blokcs")
-      console.log(infoBlock)
-    });*/
+    )
   }, [])
   
 
 
   return (
     <div>   
-        <h1> Hi, hope you like this book e-commerce.</h1>
         {
           generalInfo.map(block =>(
             <InfoBlock
+            key={block.id}
               title={block.titulo}
               subtitle={block.subtitulo}
-              image={selfie}
+              image={block.imagen}
               content={block.contenido}
             ></InfoBlock>
           ))
--- src/containers/InfoBlock.jsx
+++ src/containers/InfoBlock.jsx
@@ -7,8 +7,9 @@ const InfoBlock = ({title, subtitle, image, content}) => {
       <div className='main-cont'>
         <p className='ib-title'>{title}</p>
         <p className='ib-subtitle'>{subtitle}</p>
+        <p className='ib-content' id="ib-content-big" dangerouslySetInnerHTML={{ __html: content}}></p>
       </div>
-      <p className='ib-content' dangerouslySetInnerHTML={{ __html: content}}></p>
+      <p className='ib-content' id="ib-content-small" dangerouslySetInnerHTML={{ __html: content}}></p>
       <img src={image} alt={title}/>
     </div>
   )
