466c25522bd63a8f1212499d57180280b3877c86
unknown <angelicaguemes@hotmail.com>
2022-03-06 13:53:34 -0600

    [ADD] go to search with enter key

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -23,6 +23,16 @@ const SearchContainer = () => {
     }
   };
 
+  const handleInputChange = (val)=>{
+    setSearch(val)
+  }
+
+  const handleOnKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      redirectToSearch()
+    }
+  }
+
   return (
     <div className="SearchContainer">
       <div className="search-content">
@@ -33,12 +43,13 @@ const SearchContainer = () => {
           <input
             ref={input}
             list="categories"
-            onChange={(event) => setSearch(event.target.value)}
+            onChange={(event) => handleInputChange(event.target.value)}
+            onKeyDown={handleOnKeyDown}
             placeholder="Category, Author, Title..."
           />
           <datalist id="categories">
             {[...searchHistory, ...allCategories].map((cat) => (
-              <option value={cat} key={cat}></option>
+              <option value={cat} key={`cat${cat.id}`}></option>
             ))}
           </datalist>
           <button className="btn-search" onClick={() => redirectToSearch()}>
