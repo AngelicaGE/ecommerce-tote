548c3e92a35f1ce5861830e4fcd700400fbe5352
unknown <angelicaguemes@hotmail.com>
2022-03-07 21:09:48 -0600

    [ADD] Landing page done. Maybe add couple banners latter

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import '../styles/Landing.scss'
-import { allCategories } from '../helpers/promises'
-import {Link, NavLink } from 'react-router-dom'
+import { allCategories, allCards } from '../helpers/promises'
+import {Link } from 'react-router-dom'
+import searchIcon from '../assets/icons/search-white-24.png'
 
 const Landing = () => {
 
   return (
     <div className='Landing'>
-      <h2 className='pick-group-title rule'>Features categories</h2>
+      {/**** BANNER ****/}
+      <div className='banner-cont'>
+        <div className='banner-cont-watermark'>
+          <div className='banner-text'>
+            <p>A writter only begins a book, <br /> a reader finishes it.</p>
+          </div>
+        </div>
+      </div>
 
       {/**** CATEGORIES ****/}
+      <h2 className='pick-group-title rule'>Features categories</h2>
       <div className='categories-cont'>
         {
           allCategories.map((catElem) =>(
@@ -20,6 +29,22 @@ const Landing = () => {
           ))
         }
       </div>
+
+      {/**** SMALL CARDS ****/}
+      <hr />
+      <div className='cards-cont'>
+        {
+          allCards.map(card =>(
+            <div className='card-item' key={card.title}>
+              <img src={`images/${card.icon}`} alt={`icon ${card.title}`} />
+              <h2>{card.title}</h2>
+              <p>{card.caption}</p>
+            </div>
+          ))
+        }
+
+      </div>
+
 
     </div>
   )
--- src/helpers/promises.js
+++ src/helpers/promises.js
@@ -41,6 +41,12 @@ export const allCategories = [
     {title: "Health", category: "Health and personal growth", image: "health.jpg"}
 ]
 
+export const allCards = [
+    {title: "Free Shipping", caption: "Free shipping on all orders—no minimum purchase required", icon: "free-delivery.png"},
+    {title: "10% off in Store", caption: "Everything you see online can be found in store as well. Visit Bazarcito Lector store to get amazing discounts", icon: "tag.png"},
+    {title: "Become a member", caption: "Bazarcito members enjoy exclusive benefits, become one to enjoy them right away!", icon: "membership.png"}
+]
+
 export const ourPicks = [ 
     {category: "Romance", message: "To melt your hear"},
     {category: "Finance", message: "Lets talk about finance"},
