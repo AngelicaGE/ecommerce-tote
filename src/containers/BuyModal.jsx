7491d81e68dac97b8469f0c2a8c8f361381da941
unknown <angelicaguemes@hotmail.com>
2022-03-08 23:23:17 -0600

    [FIX] show order id on purchase complete with browser user

--- src/containers/BuyModal.jsx
+++ src/containers/BuyModal.jsx
@@ -2,122 +2,133 @@ import React, { useState, useEffect, useContext } from "react";
 import "../styles/BuyModal.scss";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase";
-import { CartContext } from '../context/CartContext'
-import { UserContext } from '../context/UserContext';
+import { CartContext } from "../context/CartContext";
+import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
-
 
 const formUser = [
   {
     label: "Name",
     name: "fname",
-    type: "text"
+    type: "text",
   },
   {
     label: "Last Name",
     name: "flastName",
-    type: "text"
+    type: "text",
   },
   {
     label: "Email",
     name: "femail",
-    type: "email"
+    type: "email",
   },
   {
     label: "Confirm Email",
     name: "fconfirmEmail",
-    type: "email"
+    type: "email",
   },
 ];
 const inputsUser = {
-    fname: "",
-    flastName: "",
-    femail: "",
-    fconfirmEmail: ""
-}
+  fname: "",
+  flastName: "",
+  femail: "",
+  fconfirmEmail: "",
+};
 
-const BuyModal = ({
-  modalStyle,
-  products,
-  setModalStyle,
-}) => {
+const BuyModal = ({ modalStyle, products, setModalStyle }) => {
   const [formFields, setFormFields] = useState([]);
   const [formFieldsInputs, setFormFieldsInputs] = useState([]);
-  const {userId, user, clearUserCart} = useContext(UserContext)
-  const { clearCart} = useContext(CartContext)
+  const { userId, user, clearUserCart } = useContext(UserContext);
+  const { clearCart } = useContext(CartContext);
 
   const [orderCompleted, setOrderCompleted] = useState(false);
   const [orderNumber, setOrderNumber] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("Please fill out every input form")
+  const [errorMessage, setErrorMessage] = useState(
+    "Please fill out every input form"
+  );
 
   useEffect(() => {
-        setFormFields(formUser)
-        setFormFieldsInputs(formFieldsInputs)
-        if(userId && user){
-            formFieldsInputs.femail = user.email;
-            formFieldsInputs.fconfirmEmail = user.email;
-            formFieldsInputs.fname = user.displayName.split(" ")[0];
-            formFieldsInputs.flastName = user.displayName.split(" ")[1];
-        }
-    }, [user])
+    setFormFields(formUser);
+    setFormFieldsInputs(formFieldsInputs);
+    if (userId && user) {
+      formFieldsInputs.femail = user.email;
+      formFieldsInputs.fconfirmEmail = user.email;
+      formFieldsInputs.fname = user.displayName.split(" ")[0];
+      formFieldsInputs.flastName = user.displayName.split(" ")[1];
+    }
+  }, [user]);
 
-  const validateForm = () =>{
-    let isValid = true
-    console.log(formFieldsInputs.fconfirmEmail)
-    if((formFieldsInputs.fconfirmEmail)  && (formFieldsInputs.femail !== formFieldsInputs.fconfirmEmail)){
-        isValid = false;
-        setErrorMessage("Email confirmation must match")
+  const validateForm = () => {
+    let isValid = true;
+    console.log(formFieldsInputs.fconfirmEmail);
+    if (
+      formFieldsInputs.fconfirmEmail &&
+      formFieldsInputs.femail !== formFieldsInputs.fconfirmEmail
+    ) {
+      isValid = false;
+      setErrorMessage("Email confirmation must match");
     }
 
     Object.entries(formFieldsInputs).forEach(([key, value]) => {
-        if(value === ""){
-            console.log(value)
-             isValid = false;
-        }
-    })
-    return isValid
-  }
+      if (value === "") {
+        console.log(value);
+        isValid = false;
+      }
+    });
+    return isValid;
+  };
 
   const handleOnComplete = async () => {
+    console.log("handleOnComplete");
     let formIsValid = validateForm();
     if (!formIsValid) {
       alert(errorMessage);
       return;
     }
     let order = {
-        orderdata: formFieldsInputs,
-        products,
-        useruid:userId
+      orderdata: formFieldsInputs,
+      products,
+      useruid: userId,
     };
-    if(userId){
-        order = {...order,useruid:userId };
+    if (userId) {
+      order = { ...order, useruid: userId };
     }
 
     const docRef = await addDoc(collection(db, "orders"), order);
     setOrderNumber(docRef.id);
     setOrderCompleted(true);
-    setFormFieldsInputs(inputsUser)
-    if(userId){
-        clearUserCart(userId)
-    }else{
-        clearCart();
+    //console.log(docRef.data());
+    if (userId) {
+      clearUserCart(userId);
+    } else {
+     // clearCart();
     }
   };
 
   const handleCloseModal = () => {
     setModalStyle("hide");
-    setFormFieldsInputs(inputsUser)
+    setFormFieldsInputs(inputsUser);
   };
 
   let navigate = useNavigate();
   const handleCloseConfirmation = () => {
+    setFormFieldsInputs(inputsUser);
+    if (userId) {
+      clearUserCart(userId);
+    } else {
+      clearCart();
+    }
+
     setModalStyle("hide");
     navigate("/");
-    setFormFieldsInputs(inputsUser)
+    setFormFieldsInputs(inputsUser);
   };
 
   const onChange = (event) => {
-    setFormFieldsInputs({ ...formFieldsInputs, [event.target.name]: event.target.value });
+    setFormFieldsInputs({
+      ...formFieldsInputs,
+      [event.target.name]: event.target.value,
+    });
   };
 
   return (
@@ -142,7 +153,11 @@ const BuyModal = ({
               <label htmlFor={input.name}>{input.label}</label>
               <input
                 type={input.type}
-                value={formFieldsInputs[input.name]? formFieldsInputs[input.name]: ''}
+                value={
+                  formFieldsInputs[input.name]
+                    ? formFieldsInputs[input.name]
+                    : ""
+                }
                 onChange={onChange}
                 name={input.name}
               />
